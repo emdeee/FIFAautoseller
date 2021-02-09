9ea6abab3cc6867bfad71f9db0a6faa361724eba
Manuel D <59400729+emdeee@users.noreply.github.com>
2021-02-09 19:28:54 +0100

    Add files via upload
    This will be the last edit to the script. 
    It generates random waiting times now between each action to properly immitate human movement. 
    This way it will be impossible for EA to detect the script running

--- clicker.py
+++ clicker.py
@@ -1,17 +1,19 @@
-import pyautogui
-import time
+import pyautogui, time
+import randomgen
 
-starting_price =  "1600"
-buy_now_price = "1700"
-amount = 1
+starting_price =  input("Please Enter the Starting Price: ")
+buy_now_price = input("Please Enter the Buy Now Price: ")
+amount = input("Please Enter the Amount of players you want to sell: ")
 
-for x in range(amount):
+for x in range(int(amount)):
     pyautogui.click(1360, 567, duration = 1)
     pyautogui.click(1377, 691, duration = 1)
-    time.sleep(0.2)
+    time.sleep(randomgen.get_random_number())
     pyautogui.typewrite(starting_price)
     pyautogui.click(1410, 755, duration = 1)
-    time.sleep(0.2)
+    time.sleep(randomgen.get_random_number())
     pyautogui.typewrite(buy_now_price)
     pyautogui.click(1415, 895, duration = 1)
-    time.sleep(2)
+    time.sleep(4*randomgen.get_random_number())
+
+input("this  is the end")
---
+++ randomgen.py
@@ -0,0 +1,7 @@
+import random
+
+
+def get_random_number():
+    x=random.random()
+    return x
+
